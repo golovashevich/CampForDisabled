1e8ce6c646c12420161de995a2ae2c48ca774b44
Maxim Golovashevich <golovashevich@gmail.com>
2013-05-14 00:54:23 +0300

    CompareOperatorAttribute: first working version with all UTs pass

---
+++ Web/Attributes/ModelClientValidationCompareRule.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace Web.Attributes {
+	public class ModelClientValidationCompareRule : ModelClientValidationRule {
+		public ModelClientValidationCompareRule(string errorMessage, string otherProperty, 
+				ValidationCompareOperator compareOperator)
+        {
+            ErrorMessage = errorMessage;
+			//TODO: Check that this does not interfere with MVC's CompareAttribute
+            ValidationType = "compare";
+            ValidationParameters["otherProperty"] = otherProperty;
+            ValidationParameters["compareOperator"] = compareOperator;
+        }
+	}
+}
