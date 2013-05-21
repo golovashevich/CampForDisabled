842d10fe79167792e9d0f271168bfab52e336e93
bigmax1601 <maximlyashenko@gmail.com>
2013-05-21 15:24:55 +0300

    Fixing the error with connection string

--- Web/Models/CampDB.cs
+++ Web/Models/CampDB.cs
@@ -5,7 +5,14 @@ namespace Camp.Models
 {
     public class CampDB : DbContext, ICampDB
     {
-        public DbSet<DriverModel> Drivers { get; set; }
+		public CampDB(bool MultipleActiveResultSets = false) {
+			//http://stackoverflow.com/questions/4867602/entity-framework-there-is-already-an-open-datareader-associated-with-this-comma
+			this.Database
+			.Connection
+			.ConnectionString += (MultipleActiveResultSets ? ";MultipleActiveResultSets=true;" : "");
+		}
+		
+		public DbSet<DriverModel> Drivers { get; set; }
         public DbSet<CamperModel> Campers { get; set; }
         public DbSet<UserModel> Users { get; set; }
 		public DbSet<CampModel> Camps { get; set; }
