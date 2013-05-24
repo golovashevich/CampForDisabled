55d3e460a281aff6ae5860dac06da16d5c55a278
Maxim Golovashevich <golovashevich@gmail.com>
2013-05-24 18:33:52 +0300

    PhoneValidator: Unit tests and server part

---
+++ Web.Tests/Attributes/PhoneValidatorTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CustomValidation.Attributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Validation;
+
+namespace Web.Tests.Attributes {
+	[TestClass]
+	public class PhoneValidatorTests {
+		private static PhoneNumberAttribute _validator;
+		private static ValidationContext _context;
+
+		[TestInitialize]
+		public void TestInit() {
+			_validator = new PhoneNumberAttribute();
+			_context = new ValidationContext(_validator);
+		}
+
+		[TestMethod]
+		public void PhoneNumbersWithSymbols() {
+			PerformChecks(new Checks<bool, string>() {
+                { true, "+7 495 555-55-55" },
+				{ true, "+7 (495) 555_55-55#" },
+                { false, "+7 (495) 555d55—55#" },
+				{ false, "+7 (495) 555-55{55#" }
+            });
+		}
+
+		[TestMethod]
+		public void PhoneNumbersWithPluses() {
+			PerformChecks(new Checks<bool, string>() {
+                { true, "+01234567 890" }, 
+                { true, "+01 2345678901" }, 
+                { false, "+ 0 12345 6789012" }, 
+                { false, "+0123456789" }, 
+                { false, "+01234" } 
+            });
+		}
+
+		[TestMethod]
+		public void PhoneNumberLength() {
+			PerformChecks(new Checks<bool, string>()  {
+                { true, "01234567890123456789" }, 
+                { true, "01234" }, 
+                { false, "0123" }, 
+                { false, "012345678901234567890" } 
+            });
+		}
+
+		private void PerformChecks(IList<Tuple<bool, string>> checks) {
+			foreach (var check in checks) {
+				Assert.AreEqual(check.Item1, null == _validator.GetValidationResult(check.Item2, _context),
+				"Phone {0} should be {1}valid", check.Item2, check.Item1 ? "" : "in");
+			}
+		}
+
+		[TestMethod]
+		public void NullPhone() {
+			Assert.IsNull(_validator.GetValidationResult(null, _context), "Null phone is valid");
+		}
+
+		[TestMethod]
+		public void EmptyPhone() {
+			Assert.IsNull(_validator.GetValidationResult(null, _context), "Empty phone is valid"); ;
+		}
+	}
+}
--- Web/Attributes/PhoneNumberAttribute.cs
+++ Web/Attributes/PhoneNumberAttribute.cs
@@ -5,40 +5,46 @@ using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Web.Mvc;
+using Validation.Attributes;
 
-namespace CustomValidation.Attributes
-{
-    public class PhoneNumberValidator : ValidationAttribute, IClientValidatable
-    {
+//Validation algorithm:
+//Remove all _()-#
+// Allowed symbols are "+" at the beginning and digits.
+// If "+" is at the beginning, there must be 11 or 12 digits.
+// Min length is 5 digits
+// Max length is 20 digits
+
+
+namespace CustomValidation.Attributes {
+	public class PhoneNumberAttribute : BaseValidationAttribute, IClientValidatable {
 		// Defaults:
 		private const int MIN_DIGITS_COUNT = 5;
 		private const int MAX_DIGITS_COUNT = 20;
+		private const string ALLOWED_WASTE_SYMBOLS = @"\s|\(|\)|\-|\—|\#|_";
 
-		public PhoneNumberValidator() : base() { }
 
+		//TODO: Different messages for each situation
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+			if (null == value) {
+				return null;
+			}
 
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (null == value) {
-                return null;
-            }
-			
-			string phone = (string) value;
+			string phone = (string)value;
+
+			phone = Regex.Replace(phone, ALLOWED_WASTE_SYMBOLS, "");
 			if (!Regex.IsMatch(phone, @"^( +)?\+?[- 0-9#()]+?$")) {
 				ErrorMessage = ValidationMessages.PhoneInvalidSymbols;
 				return new ValidationResult(ErrorMessage);
 			}
 
-			phone = phone.Trim();
 			string onlyDigitsPhone = Regex.Replace(phone, @"[^\d]", String.Empty);
 
-			if (phone.ToCharArray()[0] == '+') {
+			if (phone[0] == '+') {
 				if (onlyDigitsPhone.Length != 11 && onlyDigitsPhone.Length != 12) {
 					ErrorMessage = ValidationMessages.PhoneStartedWithPlus;
 					return new ValidationResult(ErrorMessage);
 				}
-			}
-			else {
+			} else {
 				if (onlyDigitsPhone.Length < MIN_DIGITS_COUNT || onlyDigitsPhone.Length > MAX_DIGITS_COUNT) {
 					ErrorMessage = String.Format(CultureInfo.CurrentUICulture,
 						ValidationMessages.PhoneDigitCountNotInTheRange, MIN_DIGITS_COUNT, MAX_DIGITS_COUNT);
@@ -46,12 +52,11 @@ namespace CustomValidation.Attributes
 				}
 			}
 			return ValidationResult.Success;
-        }
+		}
 
 
-		public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
-		{
+		public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context) {
 			yield return new ModelClientPhoneNumberValidatorRule(ErrorMessage);
 		}
-    }
+	}
 }
